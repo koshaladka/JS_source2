53a5fc68afc2db1d5df97db7c33da629c18efe6e
Alena Work <123@123.ru>
2022-09-21 13:17:32 +0300

    form

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,11 @@
 import modals from "./modules/modals";
 import sliders from "./modules/sliders";
+import forms from "./modules/forms";
 
 import accordion from "./modules/accordion";
 import burger from "./modules/burger";
 import drop from "./modules/drop";
+
 
 window.addEventListener('DOMContentLoaded', () =>{
     'use strict';
@@ -11,6 +13,7 @@ window.addEventListener('DOMContentLoaded', () =>{
     modals ();
     sliders ('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
     sliders ('.main-slider-item', 'vertical');
+    forms();
 
     accordion ('.accordion-heading');
     burger ('.burger-menu', '.burger');
